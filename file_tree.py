3eb72ef78b7cb89229e74523e80c0ea8c920ce54
adpat0324 <49839475+adpat0324@users.noreply.github.com>
2025-11-13 16:13:05 -0500

    Restore parent-selected defaults while keeping deselect cascade

--- file_tree.py
+++ file_tree.py
@@ -177,10 +177,6 @@ class FileTreeSelector:
 
         if node.is_file and node.file_path:
             key = self._file_checkbox_key(node.file_path)
-            if parent_selected:
-                st.session_state[key] = True
-                self.selected_files.add(node.file_path)
-
             checked = container.checkbox(
                 node.name,
                 value=st.session_state.get(key, node.file_path in self.selected_files),
@@ -204,13 +200,13 @@ class FileTreeSelector:
             # Ensure folder checkbox state is initialised prior to widget
             # creation to avoid Streamlit's post-instantiation mutation error.
             if folder_key not in self._checkbox_states:
-                self._checkbox_states[folder_key] = st.session_state.get(folder_key, False)
+                inferred_state = st.session_state.get(folder_key, False)
+                if parent_selected and not inferred_state:
+                    inferred_state = True
+                self._checkbox_states[folder_key] = inferred_state
 
             if folder_key not in st.session_state:
                 st.session_state[folder_key] = self._checkbox_states[folder_key]
-
-            if parent_selected and not st.session_state[folder_key]:
-                st.session_state[folder_key] = True
 
             folder_selected = container.checkbox("Select all", key=folder_key)
 
